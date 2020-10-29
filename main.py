5913b190bc1a72c27f47188273018204e2db8a9d
Astropolitans <paul.bokelman1@gmail.com>
2020-10-29 16:48:08 -0700

    Changes
    added product view & more

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ def landing():
 def home():
 	return render_template("acxgg.html")
 
-@app.route("/item")
-def item():
-	return render_template("itempagetemp.html")
+@app.route("/baseview")
+def baseview():
+	return render_template("base-productview.html")
+
+@app.route("/ACMain")
+def ACMain():
+	return render_template("AC-Main.html")
+
+@app.route("/ACHoodie")
+def ACHoodie():
+	return render_template("ac-hoodie.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
