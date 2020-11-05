168447aca40d4ac1d47341f45dc519965c2a4cd7
Abhijay <fornitefrogs@gmail.com>
2020-11-05 12:15:43 -0800

    my dubsotting data
    fdsfdsaf

--- data.py
+++ data.py
@@ -16,7 +16,7 @@ def abhijay():
     helloplan = "https://docs.google.com/document/d/1XaPZ--7wySEcW2FxqsHtjPz_ZBL5XkvMO3YKU3WnfuE/edit"
     flaskjournal = "https://docs.google.com/document/u/1/d/1eiWbGAY4qZC81iHmOXacXVoH7pn94lH2Zy-InckIoAM/edit"
     flaskplan = "https://docs.google.com/document/d/105jwbIr-4cbkUsLuzsVs2iT0rq5AoKy_bZxHiKmyHqU/edit"
-    video = ""
+    video = "https://www.loom.com/embed/3d091768686a44cc96eafe356808577e"
     info = {"name": name, "emoji": emoji, "hellojournal": hellojournal, "helloplan": helloplan, "flaskjournal": flaskjournal, "flaskplan": flaskplan, "video": video}
     return info
 
