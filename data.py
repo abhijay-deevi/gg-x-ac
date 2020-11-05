c2ed175d9982b29d080455168aec556c41aad113
Abhijay <fornitefrogs@gmail.com>
2020-11-05 12:48:07 -0800

    made the formatting better
    fdsafdsafds

--- data.py
+++ data.py
@@ -17,8 +17,8 @@ def abhijay():
     helloplan = "https://docs.google.com/document/d/1XaPZ--7wySEcW2FxqsHtjPz_ZBL5XkvMO3YKU3WnfuE/edit"
     flaskjournal = "https://docs.google.com/document/u/1/d/1eiWbGAY4qZC81iHmOXacXVoH7pn94lH2Zy-InckIoAM/edit"
     flaskplan = "https://docs.google.com/document/d/105jwbIr-4cbkUsLuzsVs2iT0rq5AoKy_bZxHiKmyHqU/edit"
-    video = ""
-    summary = ""
+    video = "https://www.loom.com/embed/3d091768686a44cc96eafe356808577e"
+    summary = "Most of my problems are coming from the Jinja part of it with the blocks. I don't really understand it that well, so I want to learn more about it and use in my websites. In addition to that, I wants to learn more CSS to make the website looks really good. I also want to work on the POST to make my website have user interface. Finally, I need to work a little more on the backend so that I understand how to make the full website."
     info = {"name": name, "summary": summary, "emoji": emoji, "hellojournal": hellojournal, "helloplan": helloplan, "flaskjournal": flaskjournal, "flaskplan": flaskplan, "video": video}
     return info
 
